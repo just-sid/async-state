59b8155a6d84d0f5243bc7996d07c65f80e52f7d
just-sid <be10125.17@bitmesra.ac.in>
2020-10-15 15:08:28 +0530

    Update state button

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      meaningOfLife: 47
+    }
+  }
+
+  handleClick = () => {
+    this.setState({meaningOfLife: this.state.meaningOfLife + 1})
+  }
+
+
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              {this.state.meaningOfLife}
+            </p>
+            <button
+              onClick = {this.handleClick}>
+              Update State
+            </button>
+          </header>
+        </div>
+      );
+    }
+  }
+
+
+    
+
+  
+
+
+export default App;
